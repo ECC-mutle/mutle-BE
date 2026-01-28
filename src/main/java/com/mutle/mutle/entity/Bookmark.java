b9d001867cee8427aab535def855001df9cdcfc6
jaeeun <mydlbxiu@ewhain.net>
2026-01-28 21:39:16 +0900

    [Feat] Bookmark, Music 엔티티 설계

--- src/main/java/com/mutle/mutle/entity/Bookmark.java
+++ src/main/java/com/mutle/mutle/entity/Bookmark.java
@@ -1,4 +1,42 @@
 package com.mutle.mutle.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "bookmarks")
+@Builder
 public class Bookmark {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "bookmark_id")
+    private Long bookmarkId;
+
+    @ManyToOne
+    @JoinColumn(name = "id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "bottle_id", nullable = false)
+    private Bottle bottle;
+
+    @Column(name = "bookmark_created_at", nullable = false, updatable = false)
+    private Timestamp bookmarkCreatedAt = Timestamp.valueOf(LocalDateTime.now());
+
+    @Column(name = "bookmark_expires_at", nullable = false)
+    private Timestamp bookmarkExpiresAt = Timestamp.valueOf(LocalDateTime.now().plusDays(7));
+
+    @UpdateTimestamp
+    @Column(name = "bookmark_updated_at", nullable = false)
+    private Timestamp bookmarkUpdatedAt= Timestamp.valueOf(LocalDateTime.now());
 }
--- src/main/java/com/mutle/mutle/entity/Music.java
+++ src/main/java/com/mutle/mutle/entity/Music.java
@@ -1,7 +1,39 @@
 package com.mutle.mutle.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 
 @Entity
+@Table(name = "musics")
+@Builder
 public class Music {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "music_id")
+    private Long MusicId;
+
+    @Column(name = "track_name", nullable = false, length = 200)
+    private String trackName;
+
+    @Column(name = "artist_name", nullable = false, length = 200)
+    private String artistName;
+
+    @Column(name = "album_cover_url", nullable = false, length = 500)
+    private String albumCoverUrl;
+
+    @Column(name = "music_created_at", nullable = false, updatable = false)
+    private Timestamp musicCreatedAt;
+
+    @UpdateTimestamp
+    @Column(name = "music_created_at", nullable = false)
+    private Timestamp musicUpdatedAt;
 }
