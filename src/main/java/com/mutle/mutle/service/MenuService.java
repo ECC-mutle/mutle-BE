360585113d4b5367270c28ab77d1fdec0c31032a
SeoHyeon <shyeon16@ewha.ac.kr>
2026-02-03 23:11:58 +0900

    [Refactor] 메뉴 구성 변경에 따른 친구 요청 수 카운트 구현

--- src/main/java/com/mutle/mutle/service/MenuService.java
+++ src/main/java/com/mutle/mutle/service/MenuService.java
@@ -9,6 +9,7 @@ import com.mutle.mutle.repository.FriendShipRepository;
 import com.mutle.mutle.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,20 +18,17 @@ public class MenuService {
     private final UserRepository userRepository;
     private final FriendShipRepository friendShipRepository;
 
+    @Transactional(readOnly = true)
     public MenuResponseDto getMenu(Long id) {
 
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
-        //유리병 수
-        Integer receivedBottleCount = 0; // TODO: 유리병 도메인 완성 후 교체
-
-        //친구 수
+        //친구 요청 수
         Integer friendRequestCount = friendShipRepository.countByReceiverAndFriendshipStatus(
-                user, FriendshipStatus.valueOf("REQUEST_SENT"));
+                user, FriendshipStatus.REQUEST_SENT);
 
         return MenuResponseDto.builder()
-                .receivedBottleCount(receivedBottleCount)
                 .friendRequestCount(friendRequestCount)
                 .build();
     }
