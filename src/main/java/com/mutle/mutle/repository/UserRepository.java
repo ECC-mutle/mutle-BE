ecfb567861576dce6afa110ce42a9f2638b146b1
jaeeun <mydlbxiu@ewhain.net>
2026-02-05 06:43:09 +0900

    [Feat] UserRepository 이메일 조회 메서드 추가

--- src/main/java/com/mutle/mutle/repository/UserRepository.java
+++ src/main/java/com/mutle/mutle/repository/UserRepository.java
@@ -8,7 +8,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserId(String userId);
 
+    Optional<User> findByEmail(String email);
+
     boolean existsByUserId(String userId);
 
     boolean existsByEmail(String email);
+
 }
